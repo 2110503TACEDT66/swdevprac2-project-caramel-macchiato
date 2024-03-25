3e958a17df974c3dd4262e29287d4e4ea0bb6c26
RatchanonMo <49874@yupparaj.ac.th>
2024-03-26 03:11:37 +0700

    getSpaces

---
+++ src/libs/getSpaces.tsx
@@ -0,0 +1,13 @@
+export default async function getSpaces() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch(
+    "https://presentation-day-1-caramel-macchiato.vercel.app/api/v1/workingspace"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch hospitals");
+  }
+
+  return await response.json();
+}
