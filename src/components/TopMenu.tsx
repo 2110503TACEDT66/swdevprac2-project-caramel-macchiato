fc3266697d174cbe8ab8df998b034b4e0c3d2cd1
RatchanonMo <49874@yupparaj.ac.th>
2024-03-26 15:38:11 +0700

    login

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,19 +4,25 @@ import TopMenuItem from "./TopMenuItem";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { SiStarship } from "react-icons/si";
+import { useSession } from "next-auth/react";
 
 export default async function TopMenu() {
   const session = await getServerSession(authOptions);
+
   return (
     <div className="flex justify-between items-center px-7 py-4 top-0 fixed z-10 bg-bg w-full">
       <div className="flex gap-5 items-center">
         <SiStarship className="text-blue-500" size={40} />
         <TopMenuItem title="จองเลย" href="/booking" />
       </div>
-      <TopMenuItem
-        title={session ? "Sign-Out" : "Sign-In"}
-        href={session ? "/api/auth/signout" : "/api/auth/signin"}
-      />
+      <div className="flex gap-4 items-center">
+        {session ? <p>Welcome {session?.user.name}</p> : ""}
+
+        <TopMenuItem
+          title={session ? "Sign-Out" : "Sign-In"}
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        />
+      </div>
     </div>
   );
 }
