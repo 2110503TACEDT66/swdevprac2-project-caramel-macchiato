eb40531fed8bde38cf8fc3ac8f70e70592250acd
RatchanonMo <49874@yupparaj.ac.th>
2024-03-26 22:39:10 +0700

    no more booking page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     <main>
       <Banner />
       <div className="-mt-96 pt-96  pb-20 bg-white">
-        <div className="mx-20">
+        <div className="mx-20" id="booking">
           <h1 className="text-5xl text-">Available Co-working Space</h1>
           <div className="grid grid-cols-4 gap-9 py-10">
             {spaces.data.map((item) => (
