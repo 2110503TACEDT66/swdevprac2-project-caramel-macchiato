196f7b630baa64a2acc24fe35b747fd90b35e341
TENet1010 <Nattakorn2713@gmail.com>
2024-03-30 19:26:49 +0700

    simple login test

---
+++ __test__/testcaseLogin.test.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import userLogIn from "@/libs/userLogIn";
+import getUserProfile from "@/libs/getUserProfile";
+import { screen, render, waitFor } from "@testing-library/react";
+
+describe("Remote User Log-In", () => {
+  var logInPromise: any;
+  var logInJsonResult: any;
+  var token: string;
+  var profilePromise: any;
+  var profileJsonResult: any;
+
+  beforeAll(async () => {
+    const email = "casassaddl@gmail.com";
+    const password = "123456";
+    logInPromise = userLogIn(email, password);
+    logInJsonResult = await logInPromise;
+  });
+
+  it("userLogIn must return correct results", () => {
+    expect(logInJsonResult.success).toBeTruthy();
+    
+  });
+
+});
